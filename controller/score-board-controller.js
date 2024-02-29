8640496cc2a653b1e44b58b065728d4ca5a722e5
selvamtechjays <selvam.murugaiah@techjays.com>
2024-02-29 04:51:09 +0000

    add user object id in score board schema

--- controller/score-board-controller.js
+++ controller/score-board-controller.js
@@ -5,14 +5,15 @@ const Scoreboard = require('../models/score-board');
 // Controller function to save scoreboard data
 const saveScoreboard = async (req, res) => {
   try {
-    const { score, totalQuestions, questions, selectedOptions, correctAnswers } = req.body;
+    const { score, totalQuestions, questions, selectedOptions, correctAnswers,user } = req.body;
 
     const newScoreboard = new Scoreboard({
       score,
       totalQuestions,
       questions,
       selectedOptions,
-      correctAnswers
+      correctAnswers,
+      user
     });
 
     await newScoreboard.save();
@@ -26,8 +27,11 @@ const saveScoreboard = async (req, res) => {
 
 // Controller function to get all scoreboard data
 const getAllScoreboardData = async (req, res) => {
+  const { userId } = req.query;
   try {
-    const allScoreboardData = await Scoreboard.find();
+    const filter = userId ? { user: userId } : {};
+    
+    const allScoreboardData = await Scoreboard.find(filter).populate('user');
     res.status(200).json(allScoreboardData);
   } catch (error) {
     console.error('Error getting scoreboard data:', error);
